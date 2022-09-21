ca5cb3992dd4fed1f52690f071cd9106f8a9b27f
Matteo Vullo <matteovullo61@gmail.com>
2022-09-21 19:20:43 +0200

    yes

--- Progetto_09_06_2021.cpp
+++ Progetto_09_06_2021.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+#include "Static_stack.cpp"
+#include "BST.h"
 
 class Veicolo{
 	protected:
@@ -7,10 +11,6 @@ class Veicolo{
 		
 	public:
 		Veicolo(double cilindrata) : cilindrata(cilindrata){}
-		friend operator>>(istream& os, Veicolo& v){
-			string cilindrata;
-			getline(os, v.)
-		}
 };
 
 class Auto : public Veicolo{
@@ -23,33 +23,60 @@ class Moto : public Veicolo{
 		Moto(double cilindrata) : Veicolo(cilindrata){}	
 };
 
-class Barca : pubic Veicolo{
+class Barca : public Veicolo{
 	public:
 		Barca(double cilindrata) : Veicolo(cilindrata){}	
 };
 
 int main(){
+	//punto 1
 	ifstream file("Veicolo.txt");
 	string s;
 	int n=0;
 	while(getline(file, s)){
-		i++;
+		n++;
 	}
 	Veicolo* array[n];
 	for(int i=0; i<n; i++){
 		string s1="";
 		string s2="";
 		getline(file, s1, ',');
-		getline(file, s2, '\n')
+		getline(file, s2, '\n');
 		if(s1 == "AUTO"){
-			array[i]=new Auto(atoi(s2));
+			array[i]=new Auto(atoi(s2.c_str()));
 		}
 		if(s1 == "MOTO"){
-			array[i]=new Moto(atoi(s2));
+			array[i]=new Moto(atoi(s2.c_str()));
 		}
 		if(s1 == "BARCA"){
-			array[i]=new Barca(atoi(s2));
+			array[i]=new Barca(atoi(s2.c_str()));
 		}
 	}
+	
+	//punto 2
+	Static_stack<Auto*> a(1000);
+	Static_stack<Moto*> m(1000);
+	Static_stack<Barca*> b(1000);
+	for(int i=0; i<n; i++){
+		if(typeid(array[i])==typeid(Auto)) a.push(dynamic_cast<Auto*>(array[i]));
+		if(typeid(array[i])==typeid(Moto)) m.push(dynamic_cast<Moto*>(array[i]));
+		if(typeid(array[i])==typeid(Barca)) b.push(dynamic_cast<Barca*>(array[i]));
+	}
+	
+	//punto 3
+	BST<Auto*> bst_a;
+	BST<Moto*> bst_m;
+	BST<Barca*> bst_b;
+	for(int i=0; i<n; i++){
+		if(tpyeid(array[i])==typeid(Auto)) bst_a.insert(array[i]);
+		if(tpyeid(array[i])==typeid(Moto)) bst_m.insert(array[i]);
+		if(tpyeid(array[i])==typeid(Barca)) bst_b.insert(array[i]);
+	}
+	
+	//punto 4
+	int c;
+	cout << "Inserire una cilindrata: " << endl;
+	
+	
 	
 }
